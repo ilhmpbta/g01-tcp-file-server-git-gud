a7a793916796232f44fae43041a387db0b0f3eae
Bintang Pabeta <178406185+ilhmpbta@users.noreply.github.com>
2026-03-25 00:09:59 +0700

    feat: implement broadcast to server-sync and server-thread

--- server-sync.py
+++ server-sync.py
@@ -48,16 +48,20 @@ class Server:
         self.socket.listen()
 
         self.root_dir = os.path.expanduser(root_dir)
+        self.clients = []
 
     def start(self):
         try:
             while True:
                 client_sock, client_addr = self.socket.accept()
-
+                self.clients.append(client_sock)
                 print(f"Connected by {client_addr}")
+                self.broadcast_msg(f"Client {client_addr} has connected.", exclude_sock=client_sock)
+                
                 while True:
                     data = recv_msg(client_sock)
                     if not data:
+                        self.clients.remove(client_sock)
                         break
 
                     command = data.decode()
@@ -66,12 +70,16 @@ class Server:
                         self.handle_list(client_sock)
                     elif command.startswith('/upload'):
                         filename = command.split(maxsplit=1)[1]
-                        self.handle_upload(client_sock, filename)
+                        if self.handle_upload(client_sock, filename):
+                            self.broadcast_msg(f"Client {client_addr} has uploaded {filename}", exclude_sock=client_sock)
                     elif command.startswith('/download'):
                         filename = command.split(maxsplit=1)[1]
-                        self.handle_download(client_sock, filename)  
+                        if self.handle_download(client_sock, filename):
+                            self.broadcast_msg(f"Client {client_addr} has successfully downloaded {filename}", exclude_sock=client_sock)
         except KeyboardInterrupt:
             self.socket.close()
+            for client in self.clients:
+                client.close()
 
     def handle_list(self, sock):
         filenames = []
@@ -82,14 +90,30 @@ class Server:
                     filenames.append(entry.name)
 
         send_msg(sock, ("\n".join(filenames) + "\n").encode())
+    
+    def broadcast_msg(self, message, exclude_sock=None):
+        for client in self.clients:
+            if client != exclude_sock:
+                try:
+                    send_msg(client, message.encode())
+                except (OSError, ConnectionError):
+                    pass
 
     def handle_upload(self, sock, filename):
         filepath = f"{self.root_dir}/{filename}"
-        recv_file(sock, filepath)
+        try:
+            recv_file(sock, filepath)
+            return True
+        except (OSError, IOError):
+            return False
 
     def handle_download(self, sock, filename):
         filepath = f"{self.root_dir}/{filename}"
-        send_file(sock, filepath)
+        try:
+            send_file(sock, filepath)
+            return True
+        except (OSError, IOError):
+            return False
 
 if __name__ == "__main__":
     server_port = int(input("Enter server's port: ").strip())
--- server-thread.py
+++ server-thread.py
@@ -49,33 +49,29 @@ class Server:
         self.socket.listen()
 
         self.clients = []
-
         self.root_dir = os.path.expanduser(root_dir)
 
     def run(self):
         try:
             while True:
                 client_sock, client_addr = self.socket.accept()
+                self.clients.append(client_sock)
                 print(f"Connected by {client_addr}")
+                self.broadcast_msg(f"Client {client_addr} has connected.", exclude_sock=client_sock)
 
-                client = {'client_socket': client_sock, 'client_addr': client_addr}
-
-                self.clients.append(client)
-                Thread(target = self.handle_new_client, args=(client,)).start()                    
+                Thread(target=self.handle_new_client, args=(client_sock, client_addr)).start()                    
         except KeyboardInterrupt:
             self.socket.close()
-
             for client in self.clients:
-                client['client_socket'].close()
+                client.close()
     
-    def handle_new_client(self, client):
-        client_sock = client['client_socket']
+    def handle_new_client(self, client_sock, client_addr):
         running = 1
         while running:
             data = recv_msg(client_sock)
 
             if not data:
-                self.clients.remove(client)
+                self.clients.remove(client_sock)
                 client_sock.close()
                 running = 0
                 break
@@ -86,28 +82,36 @@ class Server:
                 self.handle_list(client_sock)
             elif command.startswith('/upload'):
                 filename = command.split(maxsplit=1)[1]
-                self.handle_upload(client_sock, filename)
+                if self.handle_upload(client_sock, filename):
+                    self.broadcast_msg(f"Client {client_addr} has uploaded {filename}", exclude_sock=client_sock)
             elif command.startswith('/download'):
                 filename = command.split(maxsplit=1)[1]
-                self.handle_download(client_sock, filename)  
+                if self.handle_download(client_sock, filename):
+                    self.broadcast_msg(f"Client {client_addr} has successfully downloaded {filename}", exclude_sock=client_sock)  
     
-    def handle_list(self, sock):
-        filenames = []
-        
-        with os.scandir(self.root_dir) as entries:
-            for entry in entries:
-                if entry.is_file():
-                    filenames.append(entry.name)
-
-        send_msg(sock, ("\n".join(filenames) + "\n").encode())
-
     def handle_upload(self, sock, filename):
         filepath = f"{self.root_dir}/{filename}"
-        recv_file(sock, filepath)
+        try:
+            recv_file(sock, filepath)
+            return True
+        except (OSError, IOError):
+            return False
 
     def handle_download(self, sock, filename):
         filepath = f"{self.root_dir}/{filename}"
-        send_file(sock, filepath)
+        try:
+            send_file(sock, filepath)
+            return True
+        except (OSError, IOError):
+            return False
+    
+    def broadcast_msg(self, message, exclude_sock=None):
+        for client in self.clients:
+            if client != exclude_sock:
+                try:
+                    send_msg(client, message.encode())
+                except (OSError, ConnectionError):
+                    pass
 
 if __name__ == "__main__":
     server = Server("127.0.0.1", 6767, '~')
