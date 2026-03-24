b6af8a0d64e4636e0e6cb634be5c2a18a971ff90
lazuardiyasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2026-03-24 17:01:16 +0700

    feat: implement server-sync upload handling

--- server-sync.py
+++ server-sync.py
@@ -3,7 +3,7 @@ import struct
 import os
 
 class Server:
-    def __init__(self, HOST, PORT, root_dir="~/workspace/college/progjar/g01-tcp-file-server-git-gud"):
+    def __init__(self, HOST, PORT, root_dir):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((HOST, PORT))
@@ -28,7 +28,7 @@ class Server:
                             self.handle_list(client_sock)
                         elif command.startswith('/upload'):
                             filename = command.split(maxsplit=1)[1]
-                            self.handle_upload(client_sock)
+                            self.handle_upload(client_sock, filename)
                         elif command.startswith('/download'):
                             filename = command.split(maxsplit=1)[1]
                             self.handle_download(client_sock, filename)  
@@ -62,6 +62,17 @@ class Server:
                 sock.sendall(struct.pack(">I", len(chunk)) + chunk)
         sock.sendall(struct.pack(">I", 0))
 
+    def recv_file(self, sock, path):
+        with open(path, "wb") as f:
+            while True:
+                length = struct.unpack(">I", sock.recv(4))[0]
+                if length == 0:
+                    break
+                buffer = b""
+                while len(buffer) < length:
+                    buffer += sock.recv(length - len(buffer))
+                f.write(buffer)
+
     def handle_list(self, sock):
         filenames = []
         
@@ -74,8 +85,9 @@ class Server:
 
         self.send_msg(sock, payload)
 
-    def handle_upload(self, sock):
-        pass
+    def handle_upload(self, sock, filename):
+        filepath = f"{self.root_dir}/{filename}"
+        self.recv_file(sock, filepath)
 
     def handle_download(self, sock, filename):
         filepath = f"{self.root_dir}/{filename}"
@@ -85,4 +97,12 @@ class Server:
             self.send_file(sock, filepath)
 
 if __name__ == "__main__":
-    Server('127.0.0.1', 6767)
+    # server_ip = input("Enter server's IP: ").strip()
+    # server_port = int(input("Enter server's port: ").strip())
+    # root_dir = input("Enter server's root directory: ").strip()
+
+    server_ip = '127.0.0.1'
+    server_port = 6767
+    root_dir = '~'
+
+    Server(server_ip, server_port, root_dir)
